dcb97766d5aa4f1aeb53fdbd8cebc0b196c7a5f6
Lebrun Nya <lebrun.nya@bpso.biz>
2016-06-30 13:37:21 +0200

    Add security group for WMS Akanea Model

--- wmsImportationAKANEA/__openerp__.py
+++ wmsImportationAKANEA/__openerp__.py
@@ -5,7 +5,12 @@
     'version': '0.32.9',
     'author': 'Alain LEGRAND',
     'depends': ['base','base_setup','resource','stock','product','sale'],
-    'data': ['wizard/scheduler_wms_akanea.xml','wms_akanea_internal_mvt_view.xml','wizard/multi_import_stock_mvt_wizard.xml'],
+    'data': [
+             'wizard/scheduler_wms_akanea.xml',
+             'wms_akanea_internal_mvt_view.xml',
+             'wizard/multi_import_stock_mvt_wizard.xml',
+             'security/ir.model.access.csv',
+             ],
     'installable': True,
     'application': False,
     'auto_install': False,
